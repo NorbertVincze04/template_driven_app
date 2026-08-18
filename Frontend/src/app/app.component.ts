80b8a7f0da408f6f14da0f4cb19ecf42dfa31cdd
Vincze Norbert-Istvan (CljP/HRL-PJ) <vin3clj@bosch.com>
2026-08-18 15:04:48 +0300

    base components

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,13 +1,49 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {
+  ActivatedRoute,
+  NavigationEnd,
+  Router,
+  RouterOutlet,
+} from '@angular/router';
+import { HeaderComponent } from '../app/shared/header/header.component';
+import { FooterComponent } from '../app/shared/footer/footer.component';
+import { Subject, filter, takeUntil } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Frontend';
+  showBars = true;
+  private destroy$ = new Subject<void>();
+
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+  ) {}
+
+  ngOnInit() {
+    this.router.events
+      .pipe(
+        filter((event) => event instanceof NavigationEnd),
+        takeUntil(this.destroy$),
+      )
+      .subscribe(() => {
+        let route = this.activatedRoute;
+        while (route.firstChild) {
+          route = route.firstChild;
+        }
+        const showBars = route.snapshot.data['showBars'];
+        this.showBars = showBars !== false;
+      });
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 }
--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
+import { SignInComponent } from './core/auth/sign-in/sign-in.component';
+import { HomeComponent } from './features/home/home.component';
+import { RegisterComponent } from './core/auth/register/register.component';
+import { NotFoundComponent } from './features/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    component: SignInComponent,
+    data: { showBars: false },
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+    data: { showBars: false },
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    canActivate: [],
+    data: { showBars: true },
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
+    data: { showBars: false },
+  },
+];
