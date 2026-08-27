7e5872dc701bf8b67b586883cac95131491d9a48
Vincze Norbert-Istvan (CljP/HRL-PJ) <vin3clj@bosch.com>
2026-08-27 13:01:40 +0300

    api rate limiter

--- Backend/src/middleware/rateLimit.middleware.ts
+++ Backend/src/middleware/rateLimit.middleware.ts
@@ -1,5 +1,15 @@
 import { rateLimit } from "express-rate-limit";
 
+export const apiRateLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  limit: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+  message: {
+    message: "Too many requests. Please try again later.",
+  },
+});
+
 export const authRateLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   limit: 10, // limit each IP to 10 requests per windowMs
